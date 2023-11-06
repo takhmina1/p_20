aa91e24a7e09981f4247bc81d1abbc5a51fdb31c
your-correct-username <takhminaacom@gmail.com>
2023-11-06 21:19:31 +0600

    first commit

---
+++ file.py
@@ -0,0 +1,42 @@
+"""
+SQlite
+P4admin
+django
+postman
+{swegger}
+django rest framework
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+ 
+ 
+"""
+
+# import random
+# import string
+
+# def generate_secret_key():
+#     characters = string.ascii_letters + string.digits + string.punctuation
+#     secret_key = ''.join(random.choice(characters) for i in range(50))
+#     return secret_key
+
+# print(generate_secret_key())
